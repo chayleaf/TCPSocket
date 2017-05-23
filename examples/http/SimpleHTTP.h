bd5c4d205afe23078e220a77a55ee7ceed42d239
Ivan Pavluk <pavlukivan@outlook.com>
2017-05-23 08:14:49 +0700

    Update SimpleHTTP.h

--- examples/http/SimpleHTTP.h
+++ examples/http/SimpleHTTP.h
@@ -21,9 +21,9 @@ Connection: close\r\n\
 \r\n";
 		SocketSession wsa;
 		TCPSocket sock;
-		sock.Connect("google.ru");
-		sock.Send("GET /?gfe_rd=cr&amp;ei=rYgjWZjaIY_EXvjmhKAO HTTP/1.1\r\n\
-Host: www.google.ru\r\n\
+		sock.Connect(adr);
+		sock.Send("GET "+path+" HTTP/1.1\r\n\
+Host: "+adr+"\r\n\
 Connection: close\r\n\
 \r\n");
 		return sock.Recv();
